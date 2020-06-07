1827a867f7f6a7a0ccd41476be717794b2994f1c
Artur <faartur@gmail.com>
2020-06-07 22:24:36 +0300

    Added vue-router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 import App from './App.vue';
+import routers from './router';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routers,
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App),
 });
