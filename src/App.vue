885a76b539133e381dcbe979fb63625ad5f90f1a
Artur <faartur@gmail.com>
2020-06-10 23:09:59 +0300

    Pages content centered

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
 	<v-app>
 		<v-content>
-			<v-container fluid>
-				<router-view></router-view>
+			<v-container class="fill-height" fluid>
+				<v-row
+					align="center"
+					justify="center"
+				>
+					<v-col
+						cols="12"
+						sm="8"
+						md="4"
+					>
+						<router-view></router-view>
+					</v-col>
+				</v-row>
 			</v-container>
 		</v-content>
 	</v-app>
